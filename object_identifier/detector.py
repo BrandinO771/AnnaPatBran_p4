5a21c008c28f1a77fd7313aba2aa0313e0de7cd8
PatrickHennessey <patrickhhennessey@gmail.com>
2020-01-12 22:27:36 -0800

    cleaning up files that no longer need to be shared.

--- object_identifier/detector.py
+++ object_identifier/detector.py
@@ -2,11 +2,11 @@ from imageai.Detection.Custom import CustomObjectDetection
 
 detector = CustomObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath("model/detection_model-ex-009--loss-0011.496.h5")
-detector.setJsonPath("json/detection_config.json")
+detector.setModelPath("model/detection_model-ex-010--loss-0011.377.h5")
+detector.setJsonPath("json/detection_config_1.json")
 detector.loadModel()
 detections = detector.detectObjectsFromImage(
-    input_image="img_input/test1.jpg", output_image_path="test1-detected.jpg")
+    input_image="img_input/test2.jpg", output_image_path="img_output/test2-detected.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"],
           " : ", detection["box_points"])
